edb2a4851313c5dd83e45fc81694ff19b46c763c
Mega <syedarbabalishah@gmail.com>
2024-07-31 06:19:36 -0700

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+const nextConfig = { async rewrites() { return [{ source: '/webmail', destination: 'https://premium210.web-hosting.com:2096/' }]; } };
+
 
 export default nextConfig;
+
